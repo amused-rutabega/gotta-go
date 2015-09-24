5b9796ab318eb19582f34035d9616d3d3d4104d5
Brandon Borders <brandon@camelcased.com>
2015-09-24 11:17:45 -0700

    Add and setup ui-router

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,26 @@
 angular.module('gotta-go', [
+  'ui.router',
   'uiGmapgoogle-maps',
   'gotta-go.map'  
 ])
 
-.config(['uiGmapGoogleMapApiProvider',
-  function (GoogleMapApiProvider) {
+.config(['uiGmapGoogleMapApiProvider', '$urlRouterProvider', '$stateProvider', '$locationProvider',
+  function (GoogleMapApiProvider, $urlRouterProvider, $stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: '/app/map/map.html',
+        controller: 'MapController'
+      });
+
+    // .state('home', {
+    //   url: '/',
+    //   templateUrl: '/app/map/map.html',
+    //   controller: 'MapController'
+    // });
+
     GoogleMapApiProvider.configure({
       key: GOOGLE_API_KEY,
       libraries: 'visualization'
