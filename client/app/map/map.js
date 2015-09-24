1d63ee0dadd073f541361cb2317c26f7c193744b
Brandon Borders <brandon@camelcased.com>
2015-09-24 12:09:03 -0700

    Add marker for current position.

--- client/app/map/map.js
+++ client/app/map/map.js
@@ -1,9 +1,26 @@
 angular.module('gotta-go.map', [])
 
 .controller('MapController', function ($scope, uiGmapGoogleMapApi) {
-  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-  
-  uiGmapGoogleMapApi.then(function (maps) {
+  // TODO: Check to see if browser supports geolocation
+  navigator.geolocation.getCurrentPosition(function (position) {
+    $scope.location = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    };
 
+    $scope.map = {
+      center: $scope.location, 
+      zoom: 16
+    };
+
+    uiGmapGoogleMapApi.then(function (maps, map) {
+      var marker = $scope.location;
+      marker.id = 1;
+      marker.icon = {
+        path: maps.SymbolPath.CIRCLE,
+        scale: 10
+      };
+      $scope.markers = [marker];
+    });
   });
 });
