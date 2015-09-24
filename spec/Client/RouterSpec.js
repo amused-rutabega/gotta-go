c5d4ad5a3f8f919997cf8fc4eb554edba3ff868f
Brandon Borders <brandon@camelcased.com>
2015-09-24 15:15:09 -0700

    Finish initial client tests

--- spec/Client/RouterSpec.js
+++ spec/Client/RouterSpec.js
@@ -1,8 +1,3 @@
-// Test state `home` to render map view
-// Test to see if it properly detects browser support for geolocation
-// Test to see if we have the current coords of the user
-// Test 
-
 describe('Routing', function () {
   var $state;
   beforeEach(module('gotta-go'));
@@ -11,8 +6,8 @@ describe('Routing', function () {
     $state = $injector.get('$state');
   }));
 
-  it('Should have "/" route', function () {
+  it('should have "/" route', function () {
     expect($state.get('home')).to.be.ok();
-  })
+  });
 
 });
