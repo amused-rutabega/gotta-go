2c0af0a239d4b59ada4082c0726228341f4daaed
Brandon Borders <brandon@camelcased.com>
2015-09-28 15:29:08 -0700

    Create LayoutController tests

---
+++ spec/Client/LayoutSpec.js
@@ -0,0 +1,33 @@
+describe('Layout', function () {
+  var $scope, $rootScope, createController;
+
+  before(function () {
+    if (!navigator.geolocation) {
+      navigator.geolocation = {
+        getCurrentPosition: function () {},
+        watchPosition: function () {}
+      };
+    }
+  });
+
+  beforeEach(module('gotta-go'));
+
+  beforeEach(inject(function ($injector) {
+    $rootScope = $injector.get('$rootScope');
+    $scope = $rootScope.$new();
+
+    var $controller = $injector.get('$controller');
+
+    createController = function () {
+      return $controller('LayoutController', {
+        $scope: $scope
+      });
+    };
+  }));
+
+  it('should have a method that centers the map on the current location of the user', function () {
+    createController();
+    
+    expect($scope.centerOnLocation).to.exist;
+  });
+});
