1f14deab8cdf827e3765d7046edf1838d694b5a2
Brandon Borders <brandon@camelcased.com>
2015-09-28 15:07:21 -0700

    Fix jshint errors

--- client/app/layout/layout.js
+++ client/app/layout/layout.js
@@ -1,6 +1,6 @@
 angular.module('gotta-go.layout', [])
 
-.controller('LayoutController', function($scope, $rootScope) {
+.controller('LayoutController', function($scope) {
   $scope.centerOnLocation = function () {
     $scope.map.panTo($scope.location);
   };
