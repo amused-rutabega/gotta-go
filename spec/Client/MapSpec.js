c1e6f2d9496fcab8c14873983bb24d7e162373e1
Brandon Borders <brandon@camelcased.com>
2015-09-28 15:24:27 -0700

    Write test for change in location

--- spec/Client/MapSpec.js
+++ spec/Client/MapSpec.js
@@ -53,4 +53,39 @@ describe('Map', function () {
 
     navigator.geolocation.getCurrentPosition = original;
   });
+
+  it('should update when the current coordinates of the user change', function () {
+    var position;
+
+    navigator.geolocation.getCurrentPosition = function (cb) {
+      position = {
+        coords: {
+          latitude: 40,
+          longitude: -100
+        }
+      };
+
+      cb(position);
+    };
+
+    createController();
+
+    original = navigator.geolocation.watchPosition;
+
+    navigator.geolocation.watchPosition = function (cb) {
+      position = {
+        coords: {
+          latitude: 41,
+          longitude: -101
+        }
+      };
+
+      cb(position);
+
+      expect($scope.location.lat).to.eql(41);
+      expect($scope.location.lng).to.eql(-101);
+    };
+
+    navigator.geolocation.getCurrentPosition = original;
+  });
 });
