4fdec44a35a6c22d5588f773fb09897939538e0a
Andrew Nickell <nickell.andrew@gmail.com>
2015-09-29 11:32:58 -0700

    get now requires latitude/longitude/radius

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,10 +2,10 @@ angular.module('gotta-go.services', [])
 
 .factory('Toilets', function ($http) {
   return {
-    get: function () { //get toilet data objects from server
+    get: function (latitude, longitude, radius) { //get toilet data objects from server
       return $http({
         method: 'GET',
-        url: '/api/toilets'
+        url: '/api/toilets' //we need the gps location and the desired radius here (?latitude=___?longitude=___?radius=___).
       })
       .then(function (response) {
         return response.data;
