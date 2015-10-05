b26fe688cfe1db9b9a09bf4c9a1094ba484cdf5c
Brandon Borders <brandon@camelcased.com>
2015-10-05 13:36:17 -0700

    Remove comment

--- client/app/toilet/toilet.js
+++ client/app/toilet/toilet.js
@@ -3,7 +3,7 @@ angular.module('gotta-go.toilet', [])
 .controller('AddToiletController', function ($scope) {
   $scope.toiletOptions = {
     icon: ' ',
-    // need to work on resizing this icon
+
     labelContent: '<i class="material-icons" style="color: #009688;">place</i>',
     labelClass: 'selectorMarker',
 
