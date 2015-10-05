23b424276cf84d9f6f4bf241617279ff66fbbbf7
Brandon Borders <brandon@camelcased.com>
2015-10-05 10:38:10 -0700

    Move form view to be from side instead

--- client/app/layout/layout.js
+++ client/app/layout/layout.js
@@ -1,11 +1,31 @@
 angular.module('gotta-go.layout', [])
 
-.controller('LayoutController', function($scope, $state) {
+.controller('LayoutController', function($scope, $state, Toilets) {
+  $scope.$state = $state;
+
+  $scope.toilet = {
+    ratings: {
+      cleanliness: 1,
+      ambiance: 1,
+      accessability: 1,
+      privacy: 1,
+      familyFriendliness: 1,
+      free: true
+    }
+  };
+
   $scope.centerOnLocation = function () {
     $scope.map.panTo($scope.location);
   };
 
   $scope.toggleLocator = function () {
     $state.go('home.addToilet');
+  };
+
+  $scope.addToilet = function () {
+    Toilets.add($scope.toilet)
+    .then(function (data) {
+      console.log(data);
+    });
   };
 });
--- client/app/toilet/toilet.js
+++ client/app/toilet/toilet.js
@@ -1,27 +1,10 @@
 angular.module('gotta-go.toilet', [])
 
-.controller('AddToiletController', function ($scope, Toilets) {
-  $scope.ready = false;
-
-  $scope.toilet = {
-    ratings: {}
-  };
-
+.controller('AddToiletController', function ($scope) {
   $scope.toiletOptions = {
     icon: ' ',
     // need to work on resizing this icon
     labelContent: '<i class="material-icons" style="color: #009688;">place</i>',
     labelClass: 'selectorMarker'
-  };
-
-  $scope.showForm = function () {
-    $scope.ready = true;
-  };
-
-  $scope.addToilet = function () {
-    Toilets.add($scope.toilet)
-    .then(function (data) {
-      console.log(data);
-    });
   };
 });
