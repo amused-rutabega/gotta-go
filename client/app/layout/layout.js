6c59ae8f65eaab3747221dfbbc151024cccbb45e
Andrew Nickell <nickell.andrew@gmail.com>
2015-10-05 16:54:49 -0700

    comment layout.js for readability

--- client/app/layout/layout.js
+++ client/app/layout/layout.js
@@ -3,6 +3,7 @@ angular.module('gotta-go.layout', [])
 .controller('LayoutController', function($scope, $state, $rootScope, Toilets) {
   $scope.$state = $state;
 
+  // default values for the addtoilet view in index.html
   $scope.toilet = {
     ratings: {
       cleanliness: 1,
@@ -14,12 +15,14 @@ angular.module('gotta-go.layout', [])
     }
   };
 
+  // Boolean to register when the spinner should spin
   $scope.submittingData = false;
 
   $scope.centerOnLocation = function () {
     $scope.map.panTo($scope.location);
   };
 
+  // Pull up the addToilet state if is not the current state
   $scope.toggleLocator = function () {
     if (!$state.is('home.addToilet')) {
       $state.go('home.addToilet');
@@ -28,16 +31,19 @@ angular.module('gotta-go.layout', [])
     }
   };
 
+  // Handler for form submission of toilet to server
   $scope.addToilet = function () {
     $scope.toilet.position = $rootScope.center;
 
     $scope.submittingData = true;
 
     Toilets.add($scope.toilet)
-    .then(function (data) {
+    .then(function (data) { // when we receive a response
       $scope.submittingData = false;
 
+      // add to the array of data to render
       $rootScope.toilets.push(data.toilet);
+      // set state to default state to clear and unrender the form
       $state.go('home');
     });
   };
