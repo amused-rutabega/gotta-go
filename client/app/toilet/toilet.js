bdde3de8491f0d466a037903f7cff8ec13016da3
Andrew Nickell <nickell.andrew@gmail.com>
2015-10-05 16:59:14 -0700

    Add comments to toilet.js for readability

--- client/app/toilet/toilet.js
+++ client/app/toilet/toilet.js
@@ -2,11 +2,14 @@ angular.module('gotta-go.toilet', [])
 
 .controller('AddToiletController', function ($scope) {
   $scope.toiletOptions = {
+    // we don't want an icon since we will use a material icon as the label
     icon: ' ',
 
+    // this creates a material location svg icon
     labelContent: '<i class="material-icons" style="color: #009688;">place</i>',
     labelClass: 'selectorMarker',
 
+    // labelAnchor expects a string with an x and y offset in pixels separated by white space
     labelAnchor: '30 50',
     zIndex: 9999999999999 // Large number so that it is always ontop of the current user position marker
   };
